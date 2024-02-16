a1a0ed1abf07b887ce80ac68950e21c0950b7ade
Vladislav237 <158447907+Vladislav237@users.noreply.github.com>
2024-02-16 13:40:07 +0300

    Update работа.py

--- работа.py
+++ работа.py
@@ -2,5 +2,7 @@ a0 = float(input())
 a1 = float(input())
 if a1 > a0:
     print(a1 , "большее число")
+    print(a0 , "меньшее число")
 else:
     print(a0 , "большее число")
+    print(a1 , "меньшее число")
