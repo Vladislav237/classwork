9d9082379235b99a0ca49c232aee909809298fde
Vladislav237 <158447907+Vladislav237@users.noreply.github.com>
2024-02-20 09:29:15 +0300

    Create практическая 2.py

---
+++ практическая 2.py
@@ -0,0 +1,10 @@
+ms = float(input())
+kmch = float(input())
+kmch1 = ms /3.6
+if kmch1> kmch:
+    print("скорость в м/c больше, чем число км/ч")
+if kmch1< kmch:
+     print("скорость в км/ч больше, чем число м/с")
+if kmch1 == kmch:
+    print("скорости одинаковые")
+    
