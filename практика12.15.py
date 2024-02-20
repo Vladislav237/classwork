61d02070a28b0351095e4062db02649b786b96ce
Vladislav237 <158447907+Vladislav237@users.noreply.github.com>
2024-02-20 10:13:36 +0300

    Create практика12.15.py

---
+++ практика12.15.py
@@ -0,0 +1,13 @@
+m1 = float(input())
+m2= float(input())
+v1= float(input())
+v2= float(input())
+p1 = m1/v1
+p2 = m2/v2
+if p1> p2:
+    print("плотность 1 больше, чем плотность 2")
+if p1< p2:
+     print("плотность 1 меньше, чем плотность 2")
+if p1 == p2:
+    print("плотности одинаковые")
+    
