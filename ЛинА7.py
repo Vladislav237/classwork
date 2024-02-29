1591013a765b3a08253ee6b00f32878dd67547f1
Vladislav237 <158447907+Vladislav237@users.noreply.github.com>
2024-02-29 11:53:31 +0300

    Create ЛинА7.py

---
+++ ЛинА7.py
@@ -0,0 +1,6 @@
+#coding: utf-8                   
+a = float(input())
+b = float(input())
+c = float(input())
+d =float(input())
+print("Периметр прямоугольника",a+b+c+d,"Диагонали прямоугольника",(a**2+b**2)**0.5)
